363e6d084767e04bff9d37243831fc18901af465
Yoshi808 <135712328+Yoshi808@users.noreply.github.com>
2023-07-18 09:44:30 -0500

    Más mejoras

---
+++ script.js
@@ -0,0 +1,13 @@
+// Funciones para redireccionar a las otras páginas
+function redireccionar() {
+    window.location.href = "progreso.html";
+}
+
+function redireccionar2() {
+    window.location.href = "puntajesmundiales.html";
+}
+
+function redireccionar3() {
+// Redireccionar a la página de juego
+window.location.href = "index.html";
+}
